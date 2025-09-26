5cf111bd9ab0c27bc49753fc222c815e48b77312
Matheus-SNeves <matheusneves1503@gmail.com>
2025-09-26 07:59:32 -0300

    deixando mais enxuto

--- src/controllers/itenspedido.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-const genericController = require('./genericController');
-
-// Gera e exporta todas as funções CRUD para o modelo 'cliente'
-module.exports = genericController(prisma.cliente);
--- src/controllers/usuario.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-const genericController = require('./genericController');
-
-module.exports = genericController(prisma.usuario);
